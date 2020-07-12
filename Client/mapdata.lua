bbcf232c7da4acc3bab7edcf8f186778e1d218ac
Josh <me@joshl.io>
2020-07-12 22:04:19 +0100

    Create mapdata.lua

---
+++ Client/mapdata.lua
@@ -0,0 +1,108 @@
+local module = {
+	["North America"] = {
+		location = {
+			{
+				sizeX = 0.36,
+				sizeY = 0.15,
+				posX = 0,
+				posY = 0.04
+			},
+			{
+				sizeX = 0.2,
+				sizeY = 0.25,
+				posX = 0.14,
+				posY = 0.19
+			}
+		}
+	},
+	["South America"] = {
+		location = {
+			{
+				sizeX = 0.13,
+				sizeY = 0.36,
+				posX = 0.27,
+				posY = 0.44
+			}
+		}
+	},
+	["Africa"] = {
+		location = {
+			{
+				sizeX = 0.14,
+				sizeY = 0.4,
+				posX = 0.45,
+				posY = 0.3
+			},
+			{
+				sizeX = 0.05,
+				sizeY = 0.23,
+				posX = 0.59,
+				posY = 0.42
+			}
+		}
+	},
+	["Europe"] = {
+		location = {
+			{
+				sizeX = 0.18,
+				sizeY = 0.2,
+				posX = 0.47,
+				posY = 0.1
+			}
+		}
+	},
+	["Asia"] = {
+		location = {
+			{
+				sizeX = 0.3,
+				sizeY = 0.12,
+				posX = 0.59,
+				posY = 0.3
+			},
+			{
+				sizeX = 0.38,
+				sizeY = 0.23,
+				posX = 0.65,
+				posY = 0.07
+			},
+			{
+				sizeX = 0.19,
+				sizeY = 0.14,
+				posX = 0.75,
+				posY = 0.42
+			}
+		}
+	},
+	["Oceania"] = {
+		location = {
+			{
+				sizeX = 0.18,
+				sizeY = 0.2,
+				posX = 0.815,
+				posY = 0.56
+			}
+		}
+	},
+	["Antarctica"] = {
+		location = {
+			{
+				sizeX = 1,
+				sizeY = 0.15,
+				posX = 0,
+				posY = 0.85
+			}
+		}
+	},
+	["North Pole"] = {
+		location = {
+			{
+				sizeX = 0.1,
+				sizeY = 0.05,
+				posX = 0.45,
+				posY = 0
+			}
+		}
+	}
+}
+
+return module
